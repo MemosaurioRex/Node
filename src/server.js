c9e2ab0767fcd77994b83dd225fe6e057a528bb7
memoparra <guillermoparra79@gmail.com>
2020-05-25 16:39:50 -0400

    configuracion de mongo lista mas .env listo

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+//inicializaciones
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+
+//variables globales
+//rutas
+app.get('/', (req, res) => {
+    res.send('holi');
+})
+
+//statics files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
